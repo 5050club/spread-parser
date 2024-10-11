6326c43b794508e78dc277c391d773db2a779d83
James Byroads <james.byroads@ecstech.com>
2024-10-10 22:33:11 -0400

    added weather alerts script and saved api response

---
+++ app/get_weather_alerts.py
@@ -0,0 +1,20 @@
+import requests
+
+alerts_url = f'https://api.weather.gov/alerts/active'
+
+lon=-81.37
+lat=30.24
+
+payload = {
+    'status': 'actual',
+    'message_type': 'alert',
+    'point': f'{round(lat, 4)},{round(lon, 4)}'
+}
+
+headers = {
+    'User-Agent': '(jbnation.com, jbyroads@gmail.com)'
+}
+
+alerts = requests.get(alerts_url, payload, headers=headers)
+
+print(alerts.json())
